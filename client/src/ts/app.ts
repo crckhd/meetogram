9fa009da31fa47272024fe7f8ad911acb946965d
Mariia Levchenko <89marygreenusa@gmail.com>
2022-07-09 22:07:53 +0000

    Feature: добавлена верстка в index.html

---
+++ client/src/ts/app.ts
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.сss">
+    <link rel="stylesheet" href="./css/normalize.css"
+    <title>Meetogram</title>
+</head>
+<body>
+<section class="hero">
+        <img class="hero__icon-zap" src="./svg/zap.svg" alt="zap">
+        <h1 class="hero__title">meetogram</h1>
+        <p class="hero__subtitle">Быстрая коммуникация для эффективных людей.</p>
+</section>
+    <div class="cubes-list">
+        <div class="cubes-list__cube cubes-list__cube_1"></div>
+        <div class="cubes-list__cube cubes-list__cube_2"></div>
+        <div class="cubes-list__cube cubes-list__cube_3"></div>
+        <div class="cubes-list__cube cubes-list__cube_4"></div>
+        <div class="cubes-list__cube cubes-list__cube_5"></div>
+        <div class="cubes-list__cube cubes-list__cube_6"></div>
+        <div class="cubes-list__cube cubes-list__cube_7"></div>
+        <div class="cubes-list__cube cubes-list__cube_8"></div>
+        <div class="cubes-list__cube cubes-list__cube_9"></div>
+        <div class="cubes-list__cube cubes-list__cube_10"></div>
+        <div class="cubes-list__cube cubes-list__cube_11"></div>
+        <div class="cubes-list__cube cubes-list__cube_12"></div>
+        <div class="cubes-list__cube cubes-list__cube_13"></div>
+        <div class="cubes-list__cube cubes-list__cube_14"></div>
+        <div class="cubes-list__cube cubes-list__cube_15"></div>
+        <div class="cubes-list__cube cubes-list__cube_16"></div>
+    </div>
+</body>
+</html>
